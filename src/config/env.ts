30a21f677a0e95aa408ea2901945afa2b954921b
Aarshit Jolapara <aarshitjolapara11@gmail.com>
2026-09-15 17:13:55 +0530

    Revert "Add React application foundation"
    This reverts commit 49b606f2a506e56f451d7db3fe954a634e36ed81.

--- src/config/env.ts
+++
@@ -1,5 +0,0 @@
-const apiUrl = import.meta.env.VITE_API_URL;
-
-export const env = {
-  apiUrl: apiUrl ?? '/api',
-} as const;
--- src/pages/RegisterPage.tsx
+++
@@ -1,5 +0,0 @@
-import { AuthForm } from '../shared/components/AuthForm';
-
-export function RegisterPage() {
-  return <div className="flex justify-center py-12"><AuthForm mode="register" /></div>;
-}
